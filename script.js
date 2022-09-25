7b68ae2b08aecd3e7682916b143b7186c51b1b9f
n <n@gmail.com>
2022-09-25 20:42:00 +0300

    final

---
+++ script.js
@@ -0,0 +1,24 @@
+var a = +prompt('Количество примеров')
+for(let i = 0, i < a, i++) {
+let num1 = random
+let num2 = random
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() *(max - min + 1) + min)
+}
+var z = +prompt('Введите количество')
+for(let i = 0, i < 5, i++){
+    let z1 = 1
+    let z2 = 2
+    let z3 = 3
+    let z4 = 4
+}if
+
+function random(min, max) {
+    return Math.floor(Math.random() *(max - min + 1) + min)
+}
+
+let pr = +prompt(num1 + z + num2)
+
+
